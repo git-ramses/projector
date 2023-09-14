ec55afaa3c4d8312db6a7e05268ddc3b8ff3f802
Ramses <xramsx23@gmail.com>
2023-09-13 19:37:07 -0600

    feat: add users table

---
+++ db/migrate/20230913022823_create_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# adds users table
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest
+      t.timestamps
+    end
+  end
+end
