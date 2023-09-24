6e03b44ac99983be9322ea5e2538a162039ab0df
Ramses <xramsx23@gmail.com>
2023-09-24 15:15:43 -0600

    feat: Add sessions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# application helper
 module ApplicationHelper
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'sessions#new'
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
   resources :users, only: %i[index show edit create update]
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  post '/sign_out', to: 'sessions#destroy'
+  get '/sign_out', to: 'sessions#destroy'
 end
