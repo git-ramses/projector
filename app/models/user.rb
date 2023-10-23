1125ad26513f8af71f26551cab0e30ed5215d1d4
Ramses <xramsx23@gmail.com>
2023-10-22 19:32:45 -0600

    feat: Add support for user profile images

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # adds virtual attributes for authentication
   has_secure_password
   has_many :roles
+  has_one_attached :avatar
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 
